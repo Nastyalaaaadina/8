8e186ce9bf8bab3a4d6619dbd8f954cae7a46df3
Nastyalaaaadina <nastyaladina@yandex.ru>
2021-10-12 00:40:07 +0300

    new version

--- 1.py
+++ 1.py
@@ -1,8 +1,25 @@
-a = int(input('Ведите число: '))
-n = int(input ('Выбирите систему счисления, в которую хотите перевести число( 2 или 8): '))
-if n == 2:
-  print(bin(a)[2:])
-elif n == 8:
-  print(oct(a)[2:])
-else: 
- print ('Ошибка, вы ввели не доступную систему счисления')
+def neNol(a):
+ if a > 0:
+  return 1
+ else:
+  return 0
+try:
+ a = int(input('Ведите число: '))
+ n = int(input ('Выбирите систему счисления, в которую хотите перевести число: '))
+ while n != 2 and n != 8:
+  print('Выберете 2-ую или 8-ую систему счисления')
+  n = int(input())
+ if n == 2:
+  x = ''
+  while neNol(a) == 1:
+   x = str(a % 2) + x
+   a = a // 2
+   print(x)
+ if n == 8:
+  y = ''
+ while neNol(a) == 1:
+  y = str(a % 8) + y
+  a = a // 8
+  print(y)
+except ValueError:
+ print('Повторите попытку, вами было введено не число')
